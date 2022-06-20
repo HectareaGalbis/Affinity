78027acebdb7970ead5eb1ffc1edbebc82c485fe
Hectarea <hectometrocuadrado@gmail.com>
2022-06-20 21:24:01 +0200

    Update package.lisp

--- package.lisp
+++ package.lisp
@@ -3,5 +3,6 @@
 (defpackage "more-cffi"
   (:use :cl :alexandria :iterate)
   (:nicknames :mcffi)
-  (:export #:def-foreign-accessors
+  (:export #:defwith
+	   #:def-foreign-accessors
 	   #:def-foreign-constructor-destructor))
