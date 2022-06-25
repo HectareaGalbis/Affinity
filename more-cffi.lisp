8061646eb53e2a7db57343505eb35a410bef1d98
Hectarea <hectometrocuadrado@gmail.com>
2022-06-25 19:39:54 +0200

    Update more-cffi.lisp

--- more-cffi.lisp
+++ more-cffi.lisp
@@ -41,7 +41,7 @@
     (find-slot-names-aux slot-names expr nil)))
 
 
-;; Substitute every ocurrence of the symbol old with the symbol neweach symbol in assoc-symbol by the
+;; Substitute every ocurrence of each symbol in assoc-symbol by
 ;; its associated symbol
 (defun rec-substitute (l assoc-symbols)
   (cond
@@ -50,7 +50,7 @@
     (t l)))
 
 
-;; Defines a getter and asetter for a foreign struct slot
+;; Defines a getter and a setter for a foreign struct slot
 ;; If no getter provided, the returned value is the slot value itself
 ;; If no setter provided, the stored value is the value passed to setf
 ;; If getter is provided, this must be a list of two elements. The former must be
@@ -59,8 +59,8 @@
 ;; The resulting value from evaluating that expression is returned.
 ;; If setter is provided, this must be a list of two elements. The former must be another
 ;; list with a new-value parameter and additional parameters if needed. The latter must
-;; be an expression using this parameters and the slots from the structure. The resulting value
-;; is stored in the specified slot.
+;; be an expression using this parameters and the slots from the structure. This expression
+;; should specialize the slot assignment.
 (defmacro def-foreign-slot-accessors (getter-name slot type
 				      &key (slot-names nil) (getter nil getter-p) (setter nil setter-p))
   (let* ((arg (gensym))
@@ -94,7 +94,7 @@
 ;; (slot :getter getter-expression :setter setter-expression)
 ;; The getter-expressions and setter expressions are explained in the above macro (def-foreign-slot-accessors)
 ;; If no getter-expr or setter-expr are provided, then the correspondly function is not defined.
-(defmacro def-foreign-accessors (prefix type &rest slot-accessors)
+(defmacro def-foreign-accessors (prefix type &body slot-accessors)
   (let ((slot-names (cffi:foreign-slot-names type)))
     `(progn ,@(loop for slot-accessor in slot-accessors
 		    collect (if (symbolp slot-accessor)
@@ -127,7 +127,36 @@
 ;;                                                  on the slot to assure that the destructor dealloc correctly
 ;;                                                  the structure. This expression can use all the slots from
 ;;                                                  the structure.
-(defmacro def-foreign-constructor-destructor (suffix type &rest slot-descriptors)
+(defun check-suffix (suffix)
+  (unless (symbolp suffix)
+      (error "Expected a symbol.~%Found:~%   ~S" suffix)))
+
+(defun check-foreign-type (type)
+  (unless (and (listp type)
+	       (eq (car type) :struct)
+	       (symbolp (cadr type)))
+    (error "Expected a list (:struct type) where type is a cffi type.~%Found:~%   ~S" type)))
+
+(defun check-slot-member (slot-member slot-names struct-type)
+  (unless (member slot-member slot-names)
+    (error "Expected a slot member from ~S~%Found:~%   ~S" struct-type slot-member)))
+
+(defun check-slot-descriptors (slot-descriptors slot-names struct-type)
+  (iter (for slot-descriptor in slot-descriptors)
+    (if (listp slot-descriptor)
+	(progn
+	  (check-slot-member (car slot-descriptor) slot-names struct-type)
+	  (if (not (null (cdr slot-descriptor)))
+	      (iter (for rest-descriptor on (cdr slot-descriptor) by (lambda (x) (cdr (cdr x))))
+		(unless (member (car rest-descriptor) '(:init-form :create :destroy))
+		  (error "Expected :init-form, :create or :destroy in:~%   ~S~%Found:~%   ~S"
+			 slot-descriptor (car rest-descriptor))))))
+	(check-slot-member slot-descriptor slot-names struct-type))))
+
+(defmacro def-foreign-constructor-destructor (suffix type &body slot-descriptors)
+  (check-suffix suffix)
+  (check-foreign-type type)
+  (check-slot-descriptors slot-descriptors (cffi:foreign-slot-names type) type)
   (let* ((slot-names (cffi:foreign-slot-names type))
 	 (slot-keys  (loop for name in slot-names collect (intern (string name) "KEYWORD")))
 	 (slot-name-keys (apply #'append (mapcar #'list slot-names slot-keys)))
@@ -139,31 +168,32 @@
     (iter (for descriptor in slot-descriptors)
       (if (symbolp descriptor)
 	  (progn
-	    (collect descriptor into used-slots)
-	    (collect (getf slot-name-args descriptor) into c-slots))
-	  (progn
-	    (if (symbolp (car descriptor))
+	    (collect (list (list (getf slot-name-keys descriptor)
+				 (getf slot-name-args descriptor))
+			   0)
+	      into constructor-args)
+	    (collect descriptor into constructor-used-slots)
+	    (collect (getf slot-name-args descriptor) into final-args))
+	  (let ((init-form (member :init-form descriptor))
+		(create    (member :create descriptor))
+		(destroy   (member :destroy descriptor)))
+	    (collect (list (list (getf slot-name-keys (car descriptor))
+				 (getf slot-name-args (car descriptor)))
+			   (if init-form
+			       (cadr init-form)
+			       nil))
+	      into constructor-args)
+	    (if (cadr create)
+		(let ((final-arg (gensym)))
+		  (collect (list final-arg (rec-substitute (cadr create) slot-name-args))
+		    into let-bindings)
+		  (collect final-arg into final-args))
+		(collect (getf slot-name-args (car descriptor)) into final-args))
+	    (collect (car descriptor) into constructor-used-slots)
+	    (if (cadr destroy)
 		(progn
-		  (collect (car descriptor) into used-slots)
-		  (collect (list (list (getf slot-name-keys (car descriptor))
-				       (getf slot-name-args (car descriptor)))) into constructor-args))
-		(progn
-		  (collect (caar descriptor) into used-slots)
-		  (collect (list (list (getf slot-name-keys (caar descriptor))
-				       (getf slot-name-args (caar descriptor))) (cadar descriptor))
-		    into constructor-args)))
-	    (if (not (null (cdr descriptor)))
-		(progn
-		  (let ((c-symbol (gensym)))
-		    (collect (list c-symbol
-				   (rec-substitute (cadr descriptor) slot-name-args)) into let-bindings)
-		    (collect c-symbol into c-slots))
-		  (if (not (null (cddr descriptor)))
-		      (progn
-			(collect (caddr descriptor) into destructor-expressions)
-			(unioning (find-slot-names slot-names (caddr descriptor))
-				  into destructor-used-slots))))
-		(collect (getf slot-name-args (caar descriptor)) into c-slots))))
+		  (unioning (find-slot-names slot-names (cadr destroy)) into destructor-used-slots)
+		  (collect (cadr destroy) into destructor-expressions)))))
       (finally (let ((struct-object (gensym)))
 		 (return
 		   `(progn
@@ -171,8 +201,8 @@
 			(let (,@let-bindings
 			      (,struct-object (cffi:foreign-alloc ',type)))
 			  (memset ,struct-object 0 (cffi:foreign-type-size ',type))
-			  (cffi:with-foreign-slots (,used-slots ,struct-object ,type)
-			    (setf ,@(apply #'append (mapcar #'list used-slots c-slots))))
+			  (cffi:with-foreign-slots (,constructor-used-slots ,struct-object ,type)
+			    (setf ,@(apply #'append (mapcar #'list constructor-used-slots final-args))))
 			  (values ,struct-object)))
 		      (defun ,destructor-name (,struct-object)
 			,@(if destructor-used-slots
