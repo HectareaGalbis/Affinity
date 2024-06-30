961052244508a073245846e154baf338bc76646e
Héctor Galbis Sanchis <hectometrocuadrado@gmail.com>
2024-06-30 11:29:19 +0200

    First version of structures

--- src/structure.lisp
+++ src/structure.lisp
@@ -13,6 +13,11 @@
       (remf slot-copy :offset)
       (values slot-copy)))
 
+  (defun make-defclass (class-name super-classes slot-specifiers class-options)
+    `(defclass ,class-name ,super-classes
+       ,(mapcar #'make-defclass-slot-specifier slot-specifiers)
+       ,@class-options))
+
   (defun make-defcstruct-class-name (class-name type-class)
     (with ((defcstruct-class-name (ensure-list class-name)))
       `(,@defcstruct-class-name :class ,type-class)))
@@ -23,34 +28,48 @@
            (ctype (getf slot-options :ctype))
            (count (getf slot-options :count))
            (offset (getf slot-options :offset)))
-      `(,slot-name ,ctype ,@(when count `(:count ,count)) ,@(when offset `(:offset ,offset))))))
+      `(,slot-name ,ctype ,@(when count `(:count ,count)) ,@(when offset `(:offset ,offset)))))
+
+  (defun make-defcstruct (class-name type-class slot-specifiers)
+    `(cffi:defcstruct ,(make-defcstruct-class-name class-name type-class)
+       ,@(mapcar #'make-defcstruct-slot-specifier slot-specifiers)))
+
+  (defun make-translate-to-foreign (class-name type-class)
+    (with-gensyms (pointer obj-type instance)
+      `(defmethod cffi:translate-from-foreign (,pointer (,obj-type ,type-class))
+         (declare (ignore ,obj-type))
+         (let ((,instance (make-instance ',class-name)))
+           ,@(let ((struct-type `(:struct ,class-name)))
+               (loop for slot in (cffi:foreign-slot-names struct-type)
+                     collect `(setf (slot-value ,instance ',slot)
+                                    (cffi:foreign-slot-value ,pointer ',struct-type ',slot))))))))
+
+  (defun make-translate-into-foreign-memory (class-name type-class)
+    (with-gensyms (object obj-type pointer)
+      `(defmethod cffi:translate-into-foreign-memory ((,object ,class-name) (,obj-type ,type-class) ,pointer)
+         (declare (ignore ,obj-type))
+         ,@(let ((struct-type `(:struct ,class-name)))
+             (loop for slot in (cffi:foreign-slot-names struct-type)
+                   collect `(setf (cffi:foreign-slot-value ,pointer ',struct-type ',slot)
+                                  (slot-value ,object ',slot)))))))
+
+  (defun make-translate-aggregate-to-foreign (class-name type-class)
+    (with-gensyms (object obj-type pointer)
+      `(defmethod cffi::translate-aggregate-to-foreign ((,object ,class-name) (,obj-type ,type-class) ,pointer)
+         (cffi:translate-into-foreign-memory ,object ,obj-type ,pointer)))))
 
 
 (defmacro defcclass (class-name (&rest superclass-names) (&rest slot-specifiers) &rest class-options)
   (with-gensyms (type-class)
-    (with ((actual-class-name (make-defclass-class-name class-name)))
+    (let ((actual-class-name (make-defclass-class-name class-name)))
       `(progn
 
-         (defclass ,actual-class-name ,superclass-names
-           ,(mapcar #'make-defclass-slot-specifier slot-specifiers)
-           ,@class-options)
+         ,(make-defclass class-name superclass-names slot-specifiers class-options)
 
-         (cffi:defcstruct ,(make-defcstruct-class-name class-name type-class)
-           ,@(mapcar #'make-defcstruct-slot-specifier slot-specifiers))
+         ,(make-defcstruct class-name type-class slot-specifiers)
 
-         (defmethod cffi:translate-to-foreign ((object ,actual-class-name) (obj-type ,type-class))
-           )
+         ,(make-translate-to-foreign actual-class-name type-class)
 
-         (defmethod cffi:translate-from-foreign (pointer (obj-type ,type-class))
-           )
+         ,(make-translate-into-foreign-memory actual-class-name type-class)
 
-         (defmethod cffi:free-translated-object (pointer (obj-type ,type-class) param)
-           )
-
-         (defmethod cffi:translate-into-foreign-memory ((object list) (obj-type ,type-class) pointer)
-           )
-
-         (defmethod cffi::translate-aggregate-to-foreign ((object list) (obj-type ,type-class) pointer)
-           )
-
-         ))))
+         ,(make-translate-aggregate-to-foreign actual-class-name type-class)))))
